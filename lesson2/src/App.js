aa883e21f14c80b3eb285f9a6ca661d7d0c49366
Ostap Bagrij <bagrijostap@gmail.com>
2020-02-27 18:03:33 +0200

    add search input and finished application

--- lesson2/src/App.js
+++ lesson2/src/App.js
@@ -6,10 +6,20 @@ import Loader from './components/Loader';
 import PostsList from './components/PostsList';
 import MoreButton from './components/MoreButton';
 
+const filterPosts = (posts, filterText) => {
+  const result = posts.filter((postItem) => {
+    const { title, body } = postItem;
+    return title.includes(filterText) || body.includes(filterText);
+  });
+
+  return result
+};
+
 function App() {
 	const [posts, setPosts] = useState([]);
 	const [isLoading , setIsLoading] = useState(true);
 	const [showCount, setShowCount] = useState(10);
+	const [inputValue, setInputValue] = useState('');
 
 	useEffect(() => {
 		axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -19,16 +29,22 @@ function App() {
 			});
 	}, []);
 
-	const slised = posts.slice(0, showCount);
 	const onMoreButtonClick = () => {
 	  setShowCount(showCount + 10)
-
   };
 
-	const isButtonDisabled = showCount >= posts.length;
+	const onInputChange = (event) => {
+    setInputValue(event.target.value);
+    setShowCount(10);
+  };
+
+	const postsFiltered = filterPosts(posts, inputValue) ;
+	const slised = postsFiltered.slice(0, showCount);
+	const isButtonDisabled = showCount >= postsFiltered.length;
 
 	return(
     <div className="App">
+      <input onChange={onInputChange}/>
 	    {
 		    isLoading ?  <Loader/> : <PostsList posts={slised}/>
 	    }
